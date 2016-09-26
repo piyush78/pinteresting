ad40216be865da5dd0de1a2881344f8c07c01490
“piyush8” <peyushchowhan7@gmail.com>
2016-09-26 19:34:37 +0530

    modified application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :pins
-         validates :name, presence: true
+         validates  :name, presence: true
            #Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet,1 Number and 1 Special Character
-         validates :password, :presence => true,
-                              :on => create,
-
-                              :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A=Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/}
-      validates_presence_of :email
+        #  validates :password, :presence => true,
+        #                       :on => create,
+         #
+        #                       :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A=Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/}
+    #  validates_presence_of :email
       # validates_uniqueness_of :email
       # validates_format_of :email, :with => /.+@.\..+/i
 end
